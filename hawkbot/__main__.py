c534469b2950bc3d9f9e5fc39e2144e55b1a87c6
bmintz <bmintz@users.noreply.github.com>
2017-08-07 20:08:23 -0500

    __main__: fix config parsing

--- hawkbot/__main__.py
+++ hawkbot/__main__.py
@@ -1,15 +1,20 @@
 from . import bot
+
 from configparser import ConfigParser
+import sys
 
 def get_config(filename):
+	print(filename)
 	config = ConfigParser()
-	with open(filename) as config_file:
-		config.read(config_file)
+	config.read(filename)
 	return config
 
 
 def main():
-	config = get_config('config.ini')
-	
+	config = get_config(sys.argv[1])
+
 	bot.config = config
 	bot.run(config['login']['token'])
+
+if __name__ == '__main__':
+	main()
