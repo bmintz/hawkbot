f733fb08073a12caa2cc14f99e15369c3bdf38c4
bmintz <bmintz@users.noreply.github.com>
2017-08-07 19:31:36 -0500

    __main__: start of configuration stuff

--- hawkbot/__main__.py
+++ hawkbot/__main__.py
@@ -1,4 +1,15 @@
 from . import bot
-import sys
+from configparser import ConfigParser
 
-bot.run(sys.argv[1])
+def get_config(filename):
+	config = ConfigParser()
+	with open(filename) as config_file:
+		config.read(config_file)
+	return config
+
+
+def main():
+	config = get_config('config.ini')
+	
+	bot.config = config
+	bot.run(config['login']['token'])
