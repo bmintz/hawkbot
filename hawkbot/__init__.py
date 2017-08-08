6f528d60b67dedd2abbd8c8ad0bf8d407d0c55ce
bmintz <bmintz@users.noreply.github.com>
2017-08-07 19:23:49 -0500

    add on_ready

--- hawkbot/__init__.py
+++ hawkbot/__init__.py
@@ -3,7 +3,19 @@ import discord
 bot = discord.Client()
 
 @bot.event
+async def on_ready():
+	print('----------------------')
+	print('Logged in as:')
+	print('Username:', bot.user.name)
+	print('ID:', bot.user.id)
+	print('----------------------')
+
+
+@bot.event
 async def on_message(message):
 	if 'technically' in message.content.lower():
-		await bot.send_message(message.channel, '***Terminology Hawks, DEPLOY***')
+		await bot.send_message(
+			message.channel,
+			'***Terminology Hawks, DEPLOY***'
+		)
 
