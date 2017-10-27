918bd7b1c23bb693cbdc79d81ef1e9d8430fc9a0
bmintz <bmintz@users.noreply.github.com>
2017-10-27 00:00:05 -0500

    async2rewrite erased all my tabs 😤

--- hawkbot/__init__.py
+++ hawkbot/__init__.py
@@ -4,17 +4,17 @@ bot = discord.Client()
 
 @bot.event
 async def on_ready():
-    print('----------------------')
-    print('Logged in as:')
-    print('Username:', bot.user.name)
-    print('ID:', bot.user.id)
-    print('----------------------')
+	print('----------------------')
+	print('Logged in as:')
+	print('Username:', bot.user.name)
+	print('ID:', bot.user.id)
+	print('----------------------')
 
 @bot.event
 async def on_message(message):
-    if (bot.config['bot']['reply_to'].lower() in message.content.lower()
-    		and not is_me(message)):
-        await message.channel.send(bot.config['bot']['reply_with'])
+	if (bot.config['bot']['reply_to'].lower() in message.content.lower()
+			and not is_me(message)):
+		await message.channel.send(bot.config['bot']['reply_with'])
 
 def is_me(message):
-    return message.author == bot.user
+	return message.author == bot.user
